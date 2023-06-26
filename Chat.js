cd78074ddb222fd8694296555659aa61748a451f
ohmyoungjin <sisaudwls@naver.com>
2023-06-26 17:39:14 +0900

    20230626 :: CRUD 구조 add

--- Chat.js
+++ Chat.js
@@ -41,17 +41,74 @@ function InsertMongo(doc) {
     mongoDB.collection(collectName).insertOne(doc, function(err, res) {
             if (err) { throw err }
             else {
-                logger.info("1 document inserted");
+                logger.info("Insert Success");                
             }           
             //mdb.close();
     });   
 }
 
+/**
+ * 방에 대한 모든 정보 찾기 
+ */
 async function findAllMongo(doc) {
     let collectName = doc.RoomCode.toString();
-    let findResult = await mongoDB.collection(collectName).find({"sender": "maeng"}).toArray();
-    console.log('Found documents =>', findResult);
+    let findResult = await mongoDB.collection(collectName).find({}).toArray();
+    console.log('Found documents =>', findResult);    
+}
+
+/**
+ * 방에 대한 조건에 해당하는 값 찾기 
+ */
+async function findConditionMongo(doc) {
+    let collectName = doc.RoomCode.toString();
+    let conditionKey = doc.conditionKey.toString();        
+    let conditionValue = doc.conditionValue;
     
+    // 동적 쿼리 생성
+    let query = {};
+    query[conditionKey] = conditionValue;    
+
+    let findResult = await mongoDB.collection(collectName).find(query).toArray();
+    console.log('Found documents =>', findResult);    
+}
+
+/**
+ * 방에 대한 조건 update
+ */
+
+async function updateMongo(doc) {
+    let collectName = doc.RoomCode.toString();
+    let conditionKey = doc.conditionKey.toString();        
+    let conditionValue = doc.conditionValue;
+    let updateKey = doc.updateKey.toString();
+    let updateValue = doc.updateValue;
+    
+    // 동적 쿼리 생성
+    let query = {};
+    query[conditionKey] = conditionValue;    
+
+    let updateQuery = {};
+    updateQuery[updateKey] = updateValue;
+
+    let updateResult = await mongoDB.collection(collectName).updateOne(query, {$set: updateQuery});
+    console.log('Update documents =>', updateResult);    
+}
+
+/**
+ * 방에 대한 조건 delete
+ */
+
+async function deleteMongo(doc) {
+    let collectName = doc.RoomCode.toString();
+    let conditionKey = doc.conditionKey.toString();        
+    let conditionValue = doc.conditionValue;    
+    
+    // 동적 쿼리 생성
+    let deleteQuery = {};
+    deleteQuery[conditionKey] = conditionValue;    
+    
+    let deleteResult = await mongoDB.collection(collectName).deleteMany(deleteQuery);
+    console.log('delete documents =>', deleteResult);    
 }
 
 app.get('/', (req, res) => {
@@ -79,6 +136,42 @@ app.post('/findAll', (req, res) => {
     findAllMongo(doc);    
     res.send("select!");
 })
+
+app.post('/findCondition', (req, res) => {
+    let doc = {
+        RoomCode : req.body.RoomCode,        
+        conditionKey : req.body.condition,
+        conditionValue : req.body.conditionResult
+    }
+    logger.info("req=>" + JSON.stringify(req.body));
+    findConditionMongo(doc);    
+    res.send("select!");
+})
+
+app.post('/update', (req, res) => {
+    let doc = {
+        RoomCode : req.body.RoomCode,        
+        conditionKey : req.body.condition,
+        conditionValue : req.body.conditionResult,
+        updateKey : req.body.updateKey,
+        updateValue : req.body.updateValue
+    }
+    logger.info("req=>" + JSON.stringify(req.body));
+    updateMongo(doc);    
+    res.send("update!");
+})
+
+app.post('/delete', (req, res) => {
+    let doc = {
+        RoomCode : req.body.RoomCode,        
+        conditionKey : req.body.condition,
+        conditionValue : req.body.conditionResult,        
+    }
+    logger.info("req=>" + JSON.stringify(req.body));
+    deleteMongo(doc);    
+    res.send("delete!");
+})
+
 
 /**
  * Server start
